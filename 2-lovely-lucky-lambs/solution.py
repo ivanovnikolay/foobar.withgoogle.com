457f58a7c7af30d2290262148344ada90e4ed9db
Nikolay Ivanov <nikolay.ivanov@openmedia.co>
2021-08-02 18:43:11 +0300

    Add 2nd task

---
+++ 2-lovely-lucky-lambs/solution.py
@@ -0,0 +1,21 @@
+from itertools import takewhile
+
+
+def solution(total_lambs):
+    def fib_sum():
+        f1, f2, s = 0, 1, 0
+        while True:
+            f1, f2, s = f2, f1+f2, f2+s
+            yield s
+
+    def stingy():
+        return sum(1 for _ in takewhile(lambda s: s <= total_lambs, fib_sum()))
+
+    def generous():
+        return int.bit_length(total_lambs + 1) - 1
+
+    return stingy() - generous()
+
+
+print(solution(10))
+print(solution(143))
