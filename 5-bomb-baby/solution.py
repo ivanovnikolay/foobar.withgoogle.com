99d7dfa42391990e31d171def40fa514e5ab2217
Nikolay Ivanov <ivanoff.nikolay@gmail.com>
2021-08-03 23:33:36 +0300

    Add 5th solution

--- 5-bomb-baby/solution.py
+++ 5-bomb-baby/solution.py
@@ -1,6 +1,15 @@
-def solution(M, F):
-    raise NotImplementedError()
+def solution(x, y):
+    x, y, i = int(x), int(y), 0
+    try:
+        while (x, y) != (1, 1):
+            if x > y: x, y = y, x
+            n = y - 1 if x == 1 else y // x
+            y, i = y - n * x, i + n
+        return str(i)
+    except ZeroDivisionError:
+        return 'impossible'
 
 
-print(solution('4', '7'))
-print(solution('2', '1'))
+assert solution('4', '7') == '4'
+assert solution('2', '1') == '1'
+assert solution('2', '4') == 'impossible'
