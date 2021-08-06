4cefcf3889ee9dff6537c75b883ecf4f1aa07436
Nikolay Ivanov <ivanoff.nikolay@gmail.com>
2021-08-06 10:16:28 +0300

    code formatting

--- 6-doomsday-fuel/solution.py
+++ 6-doomsday-fuel/solution.py
@@ -33,31 +33,21 @@ class Matrix(list):
         return Matrix([[o*e for e in r] for r in self])
 
     def __invert__(self):
-        m = Matrix()
-        det = abs(self)
-        for i in range(len(self)):
-            r = []
-            for j in range(len(self)):
-                r.append((-1)**(i+j)*abs(self.minor(i, j)))
-            m.append(r)
-        return m.transpose() * Fraction(1, det)
+        return Matrix([[(-1)**(i+j)*abs(self.minor(i, j)) for j in self._N()] for i in self._N()]) \
+            .transpose() * Fraction(1, abs(self))
 
     def __abs__(self):
-        if len(self) == 1:
-            return self[0][0]
-        if len(self) == 2:
-            return self[0][0]*self[1][1] - self[0][1]*self[1][0]
+        return self[0][0] if len(self) == 1 else \
+            reduce(lambda d, i: d + ((-1)**i)*self[0][i]*abs(self.minor(0, i)), self._N(), 0)
 
-        det = 0
-        for i in range(len(self)):
-            det += ((-1)**i)*self[0][i]*abs(self.minor(0, i))
-        return det
+    def _N(self):
+        return range(len(self))
 
     def transpose(self):
         return Matrix([c for c in zip(*self)])
 
     def minor(self, i, j):
-        return Matrix([r[:j] + r[j+1:] for r in (self[:i] + self[i+1:])])
+        return Matrix([r[:j] + r[j+1:] for r in self[:i] + self[i+1:]])
 
 
 print(solution([[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0,0], [0, 0, 0, 0, 0]]))
