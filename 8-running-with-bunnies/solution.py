b59797abe143c5432719ceece14ed12d56b64722
Nikolay Ivanov <nikolay.ivanov@openmedia.co>
2021-08-27 14:26:41 +0300

    Add 8th task

---
+++ 8-running-with-bunnies/solution.py
@@ -0,0 +1,9 @@
+from sys import maxint as Inf
+
+
+def​ ​solution(times,​ ​times_limit):
+​ ​​ ​​ ​​ ​#​ ​Your​ ​code​ ​here
+
+
+assert solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1) == [1, 2]
+assert solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3) == [0, 1]
