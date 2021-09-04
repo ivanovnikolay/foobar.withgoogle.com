899eb45251b17185a8f18b26e472a9b1d127d07f
Nikolay Ivanov <nikolay.ivanov@openmedia.co>
2021-09-04 20:22:46 +0300

    Add 8th solution

--- 8-running-with-bunnies/solution.py
+++ 8-running-with-bunnies/solution.py
@@ -1,33 +1,29 @@
-from sys import maxint as Inf
-from itertools import permutations
+from itertools import permutations, product
 
 
 def solution(times, times_limit):
     bunnies = range(len(times) - 2)
-    try:
-        for u in range(len(times)):
-            print(bellman_ford(times, u))
-    except StopIteration:
+
+    negative_cicle = bellman_ford(times)
+    if negative_cicle:
         return bunnies
 
-
-def bellman_ford(G, s):
-    V = range(len(G))
-    dist = [0 if u == s else Inf for u in V]
-    edges = [(u, v, G[u][v]) for u, v in permutations(V, 2)]
-
-    for u, v, w in edges:
-        if w and dist[u] + w < dist[v]:
-            dist[v] = dist[u] + w
-
-    for u, v, w in edges:
-        if dist[u] + w < dist[v]:
-            raise StopIteration('Negative cicle')
-
-    return dist
+    for i in range(len(bunnies), 0, -1):
+        for rescued in permutations(bunnies, i):
+            path = [0] + [r+1 for r in rescued] + [len(bunnies)+1]
+            time = reduce(lambda s, p: s + times[p[0]][p[1]], zip(path, path[1:]), 0)
+            if time <= times_limit:
+                return sorted(rescued)
+    return []
 
 
-solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1)
-solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3)
-#assert solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1) == [1, 2]
-#assert solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3) == [0, 1]
+def bellman_ford(G):
+    nodes = range(len(G))
+    for node, u, v in product(nodes, repeat=3):
+        if G[u][node] + G[node][v] < G[u][v]:
+            G[u][v] = G[u][node] + G[node][v]
+    return any(G[u][u] < 0 for u in nodes)
+
+
+assert solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1) == [1, 2]
+assert solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3) == [0, 1]
